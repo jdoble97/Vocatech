c01b1918219e731ceb5be770341a006670e114ad
Sergio Garcia <firmasgi@gmail.com>
2020-12-17 23:01:16 +0100

    Apartado visual Angular Material Nosotros

---
+++ cliente/vocatech/src/app/component/nologueado/about/presentation-letter/presentation-letter.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+import { MatCardModule} from '@angular/material/card';
+@Component({
+  selector: 'app-presentation-letter',
+  templateUrl: './presentation-letter.component.html',
+  styleUrls: ['./presentation-letter.component.css']
+})
+export class PresentationLetterComponent implements OnInit {
+
+
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
